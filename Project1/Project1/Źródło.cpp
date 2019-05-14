e3a7e886ed189e0de851eae6688331e308ff9171
TrussM <marcin.truss.16@gmail.com>
2019-05-14 13:48:07 +0200

    Majonezxdd

---
+++ Project1/Project1/Źródło.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define WYRAZ 100
+char a[WYRAZ];
+
+void niewiemcotunapisac()
+{
+	cout << "Podaj pojedyncze s³owo (max 100 znaków) ";
+	cin >> a;
+
+}
+
+
+int main()
+{
+
+
+
+
+}
